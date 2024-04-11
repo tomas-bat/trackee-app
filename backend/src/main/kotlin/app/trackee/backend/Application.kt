d5c2f58556f2d8e7be2633df9209cf5fbcc92c98
Tomáš Batěk <tomas.bat@icloud.com>
2024-04-11 14:14:45 +0200

    ✨ BE: Update app config

--- backend/src/main/kotlin/app/trackee/backend/Application.kt
+++ backend/src/main/kotlin/app/trackee/backend/Application.kt
@@ -6,15 +6,9 @@ import app.trackee.backend.plugins.configureRouting
 import app.trackee.backend.plugins.configureSecurity
 import app.trackee.backend.plugins.configureSerialization
 import io.ktor.server.application.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
 import org.koin.ktor.plugin.Koin
 
-fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
-        .start(wait = true)
-}
-
+fun main(args: Array<String>) = io.ktor.server.netty.EngineMain.main(args)
 fun Application.module() {
 
     install(Koin) {
